5233da7802f04bb43f1477c8f0bc117774cfcf76
Scott Reichert <scottreichert86@gmail.com>
2017-09-15 14:59:51 -0700

    no new features, roadblock hit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
+var path1 = "You should check out Ruby Rails, PHP/Drupal, Java/Android"
+var path2 = "You Should check out CSS/Design or C#/.NET"
+var path3 = "You Should check out Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET"
+
+
+
 $(document).ready(function() {
-  $("#user-input").submit(function(event) {
+  $("form#user-input").submit(function(event) {
     var experience = parseInt($("#epicodus-exp").val());
     var timeOfDayLength = parsenInt($("#epidcodus-timeOfDay").val().length());
     var daysAvailable = $("#epicodus-available").val();
     var interest = $("#epicodus-interest").val();
     var startDay = $("input:radio[name=start-day]:checked").val();
-    var output = "";
 
-    if (startDay === "epicodus-today") {
-      output = "You should check out Ruby/Rails, PHP/Drupal, Java/Android";
-    } else if (startDay !== "epicodus-today") {
-      output = "You should check out CSS/Design or C#/.NET";
+    console.log(experience);
+    console.log(timeOfDayLength);
+    console.log(daysAvailable);
+    console.log(interest);
+    console.log(startDay);
+
+    if (experience >= 1) {
+      $("#output").show();
     }
+    event.preventDefault();
   });
 });
