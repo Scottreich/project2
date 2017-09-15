56c7d1b3c68fa3e85f31c0d5f99e1441e1aa5c7b
Scott Reichert <scottreichert86@gmail.com>
2017-09-15 15:44:09 -0700

    changed questions 1 and 2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-var path1 = "You should check out Ruby Rails, PHP/Drupal, Java/Android"
-var path2 = "You Should check out CSS/Design or C#/.NET"
-var path3 = "You Should check out Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET"
+// var path1 = "You should check out Ruby Rails, PHP/Drupal, Java/Android"
+// var path2 = "You Should check out CSS/Design or C#/.NET"
+// var path3 = "You Should check out Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET"
 
 
 
 $(document).ready(function() {
   $("form#user-input").submit(function(event) {
+      event.preventDefault();
     var experience = parseInt($("#epicodus-exp").val());
-    var timeOfDayLength = parsenInt($("#epidcodus-timeOfDay").val().length());
+    var timeOfDayLength = parsenInt($("#epidcodus-timeOfDay").val();
     var daysAvailable = $("#epicodus-available").val();
     var interest = $("#epicodus-interest").val();
     var startDay = $("input:radio[name=start-day]:checked").val();
+    var output = Go Fuck yourself
 
     console.log(experience);
     console.log(timeOfDayLength);
@@ -18,9 +20,8 @@ $(document).ready(function() {
     console.log(interest);
     console.log(startDay);
 
-    if (experience >= 1) {
       $("#output").show();
     }
-    event.preventDefault();
+
   });
 });
