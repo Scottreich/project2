7fd3eb756e4f26b264e2527280f275e1c732890d
Scott Reichert <scottreichert86@gmail.com>
2017-09-15 17:37:00 -0700

    js functionality, bug still present in output section

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
-// var path1 = "You should check out Ruby Rails, PHP/Drupal, Java/Android"
-// var path2 = "You Should check out CSS/Design or C#/.NET"
-// var path3 = "You Should check out Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET"
 
 
 
 $(document).ready(function() {
   $("form#user-input").submit(function(event) {
       event.preventDefault();
-    var experience = parseInt($("#epicodus-exp").val());
-    var timeOfDayLength = parsenInt($("#epidcodus-timeOfDay").val();
+    var experience = $("#epicodus-exp").val();
+    var timeOfDayLength = $("#epidcodus-hours").val();
     var daysAvailable = $("#epicodus-available").val();
-    var interest = $("#epicodus-interest").val();
+    var interest = $("#epicodus-age").val();
     var startDay = $("input:radio[name=start-day]:checked").val();
-    var output = Go Fuck yourself
+    var path1 = "Ruby Rails, PHP/Drupal, Java/Android";
+    var path2 = "CSS/Design or C#/.NET";
+    var path3 = "Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET";
+    // var add = experience + timeOfDayLength + daysAvailable + interest + startDay;
 
-    console.log(experience);
-    console.log(timeOfDayLength);
-    console.log(daysAvailable);
-    console.log(interest);
-    console.log(startDay);
-
-      $("#output").show();
-    }
-
+    if (experience > 5) {
+        $("#output1").show();
+        $("#span1").text(path1);
+    } else if (daysAvailable >= 1) {
+        $("#output2").show();
+        $("#span2").text(path2);
+    } else (experience < 5)
+        $("#output3").show();
+        $("#span3").text(path3);
+        // $("#span1").text(path2);
+    // } else (experience < 5) {
+    //     $("#output").show();
+    //     $("#span1").text(path3);
+  })
   });
-});
