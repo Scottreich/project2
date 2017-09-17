08979090e9d86c7422378ca61d683aa02d5eecb6
Scott Reichert <scottreichert86@gmail.com>
2017-09-17 16:27:33 -0700

    cleaning up js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 
-
-
 $(document).ready(function() {
   $("form#user-input").submit(function(event) {
       event.preventDefault();
@@ -12,20 +10,21 @@ $(document).ready(function() {
     var path1 = "Ruby Rails, PHP/Drupal, Java/Android";
     var path2 = "CSS/Design or C#/.NET";
     var path3 = "Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET";
-    // var add = experience + timeOfDayLength + daysAvailable + interest + startDay;
+    var add = "experience + timeOfDayLength + daysAvailable + interest + startDay";
 
-    if (experience > 5) {
+    if (add === 5) {
         $("#output1").show();
         $("#span1").text(path1);
-    } else if (daysAvailable >= 1) {
-        $("#output2").show();
-        $("#span2").text(path2);
-    } else (experience < 5)
-        $("#output3").show();
-        $("#span3").text(path3);
+    // } else if (daysAvailable > 1) {
+    //     $("#output1").show();
+    //     $("#span1").text(path2);
+    // } else (experience < 3)
+    //     $("#output1").show();
+    //     $("#span1").text(path3);
         // $("#span1").text(path2);
     // } else (experience < 5) {
     //     $("#output").show();
     //     $("#span1").text(path3);
-  })
+  }
   });
+});
