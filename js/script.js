181ace9bb605524a6e58d3d5e25bffcdf290000d
Scott Reichert <scottreichert86@gmail.com>
2017-09-18 22:35:34 -0700

    define all variables

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 
 $(document).ready(function() {
-  $("form#user-input").submit(function(event) {
+  $("#user-input").submit(function(event) {
       event.preventDefault();
     var experience = $("#epicodus-exp").val();
+    alert(experience);
     var timeOfDayLength = $("#epidcodus-hours").val();
+    alert(timeOfDayLength);
     var daysAvailable = $("#epicodus-available").val();
+    alert(daysAvailable);
     var interest = $("#epicodus-age").val();
+    alert(interest);
     var startDay = $("input:radio[name=start-day]:checked").val();
-    var path1 = "Ruby Rails, PHP/Drupal, Java/Android";
-    var path2 = "CSS/Design or C#/.NET";
-    var path3 = "Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET";
-    var add = "experience + timeOfDayLength + daysAvailable + interest + startDay";
+    alert(startDay);
+    // var path1 = "Ruby Rails, PHP/Drupal, Java/Android";
+    // var path2 = "CSS/Design or C#/.NET";
+    // var path3 = "Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET";
+    // var add = "experience + timeOfDayLength + daysAvailable + interest + startDay";
 
-    if (add === 5) {
-        $("#output1").show();
-        $("#span1").text(path1);
-    // } else if (daysAvailable > 1) {
+    // if (add === 5) {
+    //     $("#output1").show();
+    //     $("#span1").text(path1);
+    // // } else if (daysAvailable > 1) {
     //     $("#output1").show();
     //     $("#span1").text(path2);
     // } else (experience < 3)
@@ -25,6 +30,5 @@ $(document).ready(function() {
     // } else (experience < 5) {
     //     $("#output").show();
     //     $("#span1").text(path3);
-  }
   });
 });
