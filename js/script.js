72ae3e8f70d5fc20723496b924c51a1496485c7e
Scott Reichert <scottreichert86@gmail.com>
2017-09-24 12:48:27 -0700

    fix output and add logic

--- js/script.js
+++ js/script.js
@@ -1,34 +1,28 @@
 
 $(document).ready(function() {
-  $("#user-input").submit(function(event) {
+  $("form#user-input").submit(function(event) {
       event.preventDefault();
-    var experience = $("#epicodus-exp").val();
-    alert(experience);
-    var timeOfDayLength = $("#epicodus-hours").val();
-    alert(timeOfDayLength);
-    var daysAvailable = $("#epicodus-available").val();
-    alert(daysAvailable);
-    var interest = $("#epicodus-age").val();
-    alert(interest);
-    var startDay = $("input:radio[name=start-day]:checked").val();
-    alert(startDay);
-    // var path1 = "Ruby Rails, PHP/Drupal, Java/Android";
-    // var path2 = "CSS/Design or C#/.NET";
-    // var path3 = "Ruby Rails, PHP/Drupal, Java/Android, CSS/Design, or C#/.NET";
-    // var add = "experience + timeOfDayLength + daysAvailable + interest + startDay";
 
-    // if (add === 5) {
-    //     $("#output1").show();
-    //     $("#span1").text(path1);
-    // // } else if (daysAvailable > 1) {
-    //     $("#output1").show();
-    //     $("#span1").text(path2);
-    // } else (experience < 3)
-    //     $("#output1").show();
-    //     $("#span1").text(path3);
-        // $("#span1").text(path2);
-    // } else (experience < 5) {
-    //     $("#output").show();
-    //     $("#span1").text(path3);
+    var experience = $("#epicodus-exp").val();
+    // alert(experience);
+    var timeOfDayLength = $("#epicodus-hours").val();
+    // alert(timeOfDayLength);
+    var daysAvailable = $("#epicodus-available").val();
+    // alert(daysAvailable);
+    var interest = $("#epicodus-age").val();
+    // alert(interest);
+    var startDay = $("#epicodus-start").val();
+    if (experience > 5) {
+      $(".hideTracks").hide();
+      $("#trackOne").show();
+    } else if (timeOfDayLength < 5) {
+      $(".hideTracks").hide();
+      $("#trackTwo").show();
+    } else {
+      $(".hideTracks").hide();
+      $("#trackThree").show();
+
+    }
+
   });
 });
